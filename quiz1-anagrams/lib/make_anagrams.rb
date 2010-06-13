d8842079b3fa14bafbc679bde332dd8101769ebb
Niko Felger <niko.felger@gmail.com>
2010-06-13 01:24:53 +0100

    Bad ass-speed with Hashes

--- quiz1-anagrams/lib/make_anagrams.rb
+++ quiz1-anagrams/lib/make_anagrams.rb
@@ -1,24 +1,23 @@
 class MakeAnagrams
-  def initialize(word_list, debug)
-    @word_list = word_list.map{|word| word.chomp}
+  def initialize(word_list)
+    @word_list = word_list.map{|word| word.chomp}.sort
     @word_list.reject!{|word| illegal?(word)}
-    @debug = debug
   end
   
   def formatted_anagram_dictionary
     output = []
-    anagrams.keys.sort.each do |entry|
-      anagram_set = anagrams[entry].sort
+    anagrams.each do |word, anagram_set|
       case anagram_set.size
-      when 0
-        next
       when 1
-        output << ([entry] + anagram_set).join(' ')
+        next
+      when 2
+        output << [word, anagram_set.detect{|a| a != word}].join(' ')
       else
-        if entry[0] < anagram_set.first[0]
-          output << ([entry] + anagram_set).join(' ')
+        anagram_set = [word] + (anagram_set - [word])
+        if anagram_set[0][0] < anagram_set[1][0]
+          output << anagram_set.join(' ')
         else
-          output << entry + " (See '#{anagram_set.first}')"
+          output << anagram_set[0] + " (See '#{anagram_set[1]}')"
         end
       end
     end
@@ -26,43 +25,32 @@ class MakeAnagrams
   end
   
   def anagrams
-    @dictionary ||= begin
-      dictionary = {}
-      words_of_size.keys.sort.each do |n|
-        while word = words_of_size[n].shift
-          STDERR.puts word if @debug
-          next if dictionary[word]
-          
-          anagram_set = words_of_size[n].select{|candidate| anagrams?(word, candidate)}
-          dictionary[word] = anagram_set
-          
-          anagram_set.each do |anagram|
-            dictionary[anagram] = [word] + (anagram_set - [anagram])
-          end
-        end
+    @anagrams ||= begin
+      @word_list.map do |word|
+        [word, words[representative(word)]]
       end
-      dictionary
     end
   end
   
   private
   
-  def words_of_size
+  def words
     @words ||= begin
       words = {}
-      @word_list.each do |word|
-        words[word.size] ||= []
-        words[word.size] << word
+      @word_list.each do |word|        
+        rep = representative(word)
+        words[rep] ||= []
+        words[rep] << word
       end
       words
     end
   end
   
-  def illegal?(word)
-    word =~ /[^a-z]/i
+  def representative(word)
+    word.downcase.unpack('c*').sort.join('')
   end
   
-  def anagrams?(some_word, another_word)
-    some_word.downcase.split('').sort == another_word.downcase.split('').sort
+  def illegal?(word)
+    word =~ /[^a-z]/i
   end
 end
